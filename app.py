e63f77acbcef5f29cbf07b34617c1405ce94703c
mtariq6054 <mtariq6054900@gmail.com>
2024-05-15 12:21:05 +0500

    some customizations

--- app.py
+++ app.py
@@ -8,59 +8,42 @@ custom_css = """
 .stButton>button:hover {
     background-color: #2980b9 !important;
 }
-.stTextInput>div>div>textarea {
-    background-color: rgba(255, 255, 255, 0.7) !important;
-    color: black !important;
-    border-radius: 10px;
-    padding: 20px;
-    border: 4px solid #2980b9 !important;
-}
-.stTextInput>div>div>textarea::placeholder {
-    color: gray;
-    font-style: italic;
-}
+
 .prediction-result {
-    padding: 15px;
+    padding: 12px;
     border: 3px solid #2980b9;
     border-radius: 10px;
     background-color: #ecf0f1;
     font-weight: bold;
-    font-family : 'Time New Roman';
+    font-family: 'Time New Roman';
 }
 .prediction-button {
     font-weight: bold !important;
-    font-family:  'Time New Roman' ;
+    font-family: 'Time New Roman';
     font-size: 40px;
     font-weight: bold;
 }
-.prediction-probability {
-    margin-top: 10px;
-    font-size: 20px;
-    font-weight: bold;
-    color: #2980b9;
-    border: 2px solid #2980b9;
-    border-radius: 5px;
-    padding: 5px 10px;
-    background-color: #ecf0f1;
-}
+
 .title {
-    font-family:  'Time New Roman' ;
+    font-family: 'Time New Roman';
     font-size: 40px;
     font-weight: bold;
     text-align: center;
     color: #074173;
 }
 .subheader {
-    font-family: 'Time New Roman' ;
+    font-family: 'Time New Roman';
     font-size: 27px;
     font-weight: bold;
     color: #074173;
 }
+
 </style>
 """
 st.markdown(custom_css, unsafe_allow_html=True)
 
-page_bg_img = f"""
+
+page_bg_img = f"""  
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://www.wallpapertip.com/wmimgs/35-358745_thumb-image-email-background-image-hd.jpg");
@@ -107,12 +90,19 @@ st.markdown('<h1 class="title">Spam Email Classifier</h1>', unsafe_allow_html=Tr
 st.markdown('<h2 class="subheader">Enter your email text here:</h2>', unsafe_allow_html=True)
 email_text = st.text_area("", placeholder="Type your email here...")
 
+
 # Button to trigger prediction
 if st.button("Predict", key="predict_button"):
     spam_probability = predict_spam(email_text)
-    st.write("Spam Probability:", spam_probability)
-
-    # Present the prediction result
+    # Ensure the spam probability falls within the range of 0 to 1
+    spam_probability = max(0, min(spam_probability, 1))
+    # Determine the result text based on the spam probability
     st.subheader("Prediction Result:")
-    result_text = "This email is likely spam." if spam_probability > 0.7 else "This email is likely not spam."
+    if spam_probability > 0.7:
+        result_text = f"This email is likely spam with a probability of {spam_probability:.2f}."
+    else:
+        result_text = f"This email is likely not spam with a probability of {spam_probability:.2f}."
     st.markdown(f'<div class="prediction-result">{result_text}</div>', unsafe_allow_html=True)
+
+
+
